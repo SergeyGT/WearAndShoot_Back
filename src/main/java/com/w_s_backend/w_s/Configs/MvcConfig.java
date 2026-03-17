70f6bac4d7c95150a334b8c30a2ce76915ff70dd
SergeyGT <sergeyburdenyuk@mail.ru>
2026-03-18 00:29:45 +0300

    Update: Добавлен бин для RestTemp

--- src/main/java/com/w_s_backend/w_s/Configs/MvcConfig.java
+++ src/main/java/com/w_s_backend/w_s/Configs/MvcConfig.java
@@ -1,6 +1,8 @@
 package com.w_s_backend.w_s.Configs;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -11,5 +13,10 @@ public class MvcConfig implements WebMvcConfigurer {
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/").setViewName("home");
         registry.addViewController("/login");
+    }
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
     }
 }
